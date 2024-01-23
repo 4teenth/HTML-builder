89d1c19d6027707d7307a0e29a97c302ec26edbc
4teenth <aio.ao91@gmail.com>
2024-01-23 07:47:07 +0200

    feat: add info

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+
+// const fs = require('fs');
+
+// const targetPath = './03-files-in-folder/secret-folder';
+
+// fs.readdir(targetPath, function (err, files) {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   files.forEach(function (file) {
+//     console.log(file);
+//   });
+// });
+
+//* Приветствую тебя, мой проверяющий!
+//* Были определённые трудности, не успел со всем разобраться...
+//* Если не трудно и имеется такая возможность, прошу проверить невыполненные работы ближе к концу кросс-чека. Спасибо!
