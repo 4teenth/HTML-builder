1b518f1182ef7decbd5088db875c0395c26730d3
4teenth <aio.ao91@gmail.com>
2024-01-25 16:22:02 +0200

    feat: add solution for task 04, add expl for paths

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,4 +1,27 @@
 /* eslint-disable prettier/prettier */
-//* Приветствую тебя, мой проверяющий!
-//* Были определённые трудности, не успел со всем разобраться...
-//* Если не трудно и имеется такая возможность, прошу проверить невыполненные работы ближе к концу кросс-чека. Спасибо!
+const fs = require('fs');
+
+const fromDir = './04-copy-directory/files/';
+const toDir = './04-copy-directory/files-copy'; // this path without nested catalog structure, so later when copy smth to this dir do not forget about path separator
+
+// Create a folder 'files-copy' from path of toDir variable if it does not exist
+fs.mkdir(toDir, { recursive: true }, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    // Read the contents of the 'files' folder.
+    fs.readdir(fromDir, (err, files) => {
+      if (err) {
+        console.error(err);
+        return; //---
+      } else {
+        //* Alt to classic for of loop
+        files.forEach((file) => {
+          fs.copyFile(fromDir + file, toDir + '/' + file, (err) => {
+            err ? console.log(err) : console.log(file + ' - file was created');
+          });
+        });
+      }
+    });
+  }
+});
